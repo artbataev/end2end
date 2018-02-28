43b42a974988f3f34369f005f105d0385a17d989
artbataev <artbataev@gmail.com>
2018-03-01 02:13:37 +0300

    Cleanup

--- pytorch_end2end/modules/warp_ctc_wrapper.py
+++ pytorch_end2end/modules/warp_ctc_wrapper.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 
 from warpctc_pytorch import _CTC
 
+
 class WarpCTCLoss(nn.Module):
     def __init__(self, reduce=False):
         super(WarpCTCLoss, self).__init__()
@@ -19,11 +20,12 @@ class WarpCTCLoss(nn.Module):
         :return:
         """
 
+        # warp_ctc: logits: sequence_length * batch_size * num_labels, targets: flat
         batch_size = targets_lengths.size()[0]
         targets_flat = torch.cat([targets[i, :targets_lengths[i].data] for i in range(batch_size)])
 
-        # warp_ctc: logits: sequence_length * batch_size, targets: flat
         costs = self.ctc(logits.transpose(0, 1), targets_flat, logits_lengths, targets_lengths, self.size_average)
+
         if self.reduce:
             return costs.sum()
         return costs
