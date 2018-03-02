44bc3fff38ab9c1955510ee3e5509efb02d24fc4
artbataev <artbataev@gmail.com>
2018-03-03 00:58:38 +0300

    Added to_lower for Turkish

---
+++ pytorch_end2end/encoders/utils.py
@@ -0,0 +1,14 @@
+TUR_CHARS = "abcçdefgğhıijklmnoöprsştuüvyz'"
+
+def tur_to_lower(text):
+    global TUR_CHARS
+    words = text.split()
+    words_pure = []
+    for word in words:
+        if word.lower() in ["<spoken_noise>", "<silence>"]:
+            continue
+        word = word.replace("\u0130", "i").replace("I", "\u0131").casefold()
+        word = word.replace("q", "g").replace("x", "h").replace("\u0430", "a").replace("w", "v")
+        pure_word = "".join([c for c in word if c in TUR_CHARS])
+        words_pure.append(pure_word)
+    return " ".join(words_pure)
