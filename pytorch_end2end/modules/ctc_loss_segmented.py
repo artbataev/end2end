a73d3e4b5bd5b9faa0be05269aec9f7178679872
artbataev <artbataev@gmail.com>
2018-03-16 00:11:22 +0300

    Bugfixes

--- pytorch_end2end/modules/ctc_loss_segmented.py
+++ pytorch_end2end/modules/ctc_loss_segmented.py
@@ -46,6 +46,7 @@ class CTCLossSegmented(nn.Module):
         batch_ids_new = []
 
         indices_to_segment = [[0, ] for _ in range(batch_size)]
+        num_segments = 0
         for i in range(batch_size):
             start_space = -1
             all_word_well_recognized = True
@@ -59,64 +60,69 @@ class CTCLossSegmented(nn.Module):
                         if start_space != -1 and \
                                 (len(indices_to_segment[i]) == 0 or indices_to_segment[i][-1] != start_space):
                             indices_to_segment[i].append(start_space)
+                            num_segments += 1
                         if t > 0:
                             indices_to_segment[i].append(t)
+                            num_segments += 1
                     start_space = t
                     all_word_well_recognized = True
             if indices_to_segment[i][-1] != logits_lengths.data[i] - 1:
                 indices_to_segment[i].append(logits_lengths.data[i] - 1)
+                num_segments += 1
+
+        if num_segments == batch_size:
+            # no new segments found
+            loss = self.ctc(logits, targets, logits_lengths, targets_lengths)
+            return loss
+
+        logits_new = Variable(torch.zeros(num_segments, logits.size()[1], logits.size()[2]), requires_grad=False)
+        if logits.is_cuda:
+            logits_new = logits_new.cuda(logits.get_device())
+        targets_new = Variable(torch.LongTensor(num_segments, targets.size()[1]))
+        batch_ids_new = []
+
+        new_i = 0
         for i in range(batch_size):
             if len(indices_to_segment[i]) <= 2:  # use full segment
-                logits_new.append(logits[i])
+                logits_new[new_i] = logits[i]
                 batch_ids_new.append(i)
-                targets_new.append(targets[i])
+                targets_new[new_i] = targets[i]
                 targets_lengths_new.append(targets_lengths.data[i])
                 logits_lengths_new.append(logits_lengths.data[i])
+                new_i += 1
             else:
                 for k, start in enumerate(indices_to_segment[i][:-1]):
                     if targets_aligned[i, start] == self.space_idx:
                         batch_ids_new.append(i)
-                        targets_new.append(targets_aligned[i, start:start + 1])  # not aligned!
-                        logits_new.append(logits[i, start:start + 1])
+                        targets_new[new_i, 0] = targets_aligned[i, start]
+                        logits_new[new_i, 0:1] = logits[i, start:start + 1]
                         logits_lengths_new.append(1)
                         targets_lengths_new.append(1)
                         start += 1
+                        new_i += 1
                     next = indices_to_segment[i][k + 1]
                     if k < len(indices_to_segment[i]) - 2 and targets_aligned[i][next] == self.space_idx:
                         next -= 1
                     if next <= start:
                         continue
                     batch_ids_new.append(i)
-                    logits_new.append(logits[i, start:next])
+                    logits_new[new_i, :(next-start)] = logits[i, start:next]
                     current_targets = Variable(torch.LongTensor(
                         [c for c, _ in itertools.groupby(targets_aligned[i, start:next].tolist()) if
                          c != self.blank_idx]))
-                    targets_new.append(current_targets)
-                    logits_lengths_new.append(next - start + 1)
+                    targets_new[new_i, :current_targets.size[0]] = current_targets
+                    logits_lengths_new.append(next - start)
                     targets_lengths_new.append(current_targets.size()[0])
+                    new_i += 1
 
-        new_batch_size = len(batch_ids_new)
-        max_logits_len = max(logits_lengths_new)
-        max_targets_len = max(targets_lengths_new)
-
-        logits_new_var = Variable(torch.zeros(new_batch_size, max_logits_len, logits.size()[2]), requires_grad=True)
-        if logits.is_cuda:
-            logits_new_var = logits_new_var.cuda(logits.get_device())
-        targets_new_var = Variable(torch.LongTensor(new_batch_size, max_targets_len), requires_grad=False)
-        if logits.is_cuda:
-            logits_new_var = logits_new_var.cuda(logits_logsoftmax.get_device())
-
-        for i in batch_ids_new:
-            logits_new_var[i, :logits_lengths_new[i]] = logits_new[i]
-            targets_new_var[i, :targets_lengths_new[i]] = targets_new[i]
         logits_lengths_new = Variable(torch.LongTensor(logits_lengths_new), requires_grad=False)
         targets_lengths_new = Variable(torch.LongTensor(targets_lengths_new), requires_grad=False)
 
-        segmented_loss = self.ctc(logits_new_var, targets_new_var, logits_lengths_new, targets_lengths_new)
+        segmented_loss = self.ctc(logits_new, targets_new, logits_lengths_new, targets_lengths_new)
         loss = torch.zeros(batch_size)
         if logits.is_cuda:
             loss = loss.cuda(logits.get_device())
-        loss = Variable(loss, requires_grad=True)
+        loss = Variable(loss, requires_grad=False)
         for new_i, i in enumerate(batch_ids_new):
-            loss[i] += segmented_loss[new_i]
+            loss[i] = segmented_loss[new_i] + loss[i]
         return loss
