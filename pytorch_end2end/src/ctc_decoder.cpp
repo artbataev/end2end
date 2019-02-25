30daec68ed6ffe9aaa3d41e39791471cb219b1a5
artbataev <artbataev@gmail.com>
2019-02-26 00:31:17 +0300

    CTCDecoder: use static_cast for lm. Fix kenlm linking.

--- pytorch_end2end/src/ctc_decoder.cpp
+++ pytorch_end2end/src/ctc_decoder.cpp
@@ -63,8 +63,8 @@ CTCDecoder::CTCDecoder(int blank_idx_, int beam_width_ = 100,
         auto enumerate_vocab = CustomEnumerateVocab{};
         lm::ngram::Config config;
         config.enumerate_vocab = &enumerate_vocab;
-        lm_model = std::unique_ptr<lm::ngram::ProbingModel>(
-                dynamic_cast<lm::ngram::ProbingModel *>(
+        lm_model = std::unique_ptr<lm::ngram::Model>(
+                static_cast<lm::ngram::Model *>(
                         lm::ngram::LoadVirtual(lm_path.c_str(), config, lm::ngram::PROBING)));
         if (case_sensitive) {
             word2index = enumerate_vocab.get_word2index();
