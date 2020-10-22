8015aeca975df1b8232e9d5145cb0464f532833b
artbataev <artbataev@gmail.com>
2020-10-22 21:15:34 +0300

    fix __init__.py

--- pytorch_end2end/__init__.py
+++ pytorch_end2end/__init__.py
@@ -1,5 +1,6 @@
-from .decoders.ctc_decoder import CTCDecoder
-from .modules.ctc_loss import CTCLoss
-from .encoders.text_encoders import CTCEncoder
+import torch  # noqa
+from pytorch_end2end.decoders.ctc_decoder import CTCDecoder
+from pytorch_end2end.modules.ctc_loss import CTCLoss
+from pytorch_end2end.encoders.text_encoders import CTCEncoder
 
 __all__ = ["CTCLoss", "CTCDecoder", "CTCEncoder"]
